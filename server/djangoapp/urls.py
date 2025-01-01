a1894eb0e42d279240f9b68ebc8457ab670865f1
Dulshan The Developer <abavira@gmail.com>
2025-01-01 21:55:15 +0530

    get cars

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -9,10 +9,11 @@ from . import views
 app_name = "djangoapp"
 urlpatterns = [
     # # path for registration
-    path(route='register', view=views.registration, name='registration'),
+    path(route="register", view=views.registration, name="registration"),
     # path for login
-    path(route='login', view=views.login_user, name='login'),
-    path(route='logout', view=views.logout_request, name='logout'),
+    path(route="login", view=views.login_user, name="login"),
+    path(route="logout", view=views.logout_request, name="logout"),
+    path(route="get_cars", view=views.get_cars, name="getcars"),
     # path for dealer reviews view
     # path for add a review view
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
